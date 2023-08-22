f60bf99721d73c8beb6d69423011b0f36c7913c1
Marta N.- Data Analyst <116729581+Marta-BDA@users.noreply.github.com>
2023-08-22 11:31:08 +0200

    Update principal.py

--- principal.py
+++ principal.py
@@ -3,7 +3,8 @@ import streamlit
 streamlit.title ('Zenas catalog')
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+snowflake_credentials = st.secrets["snowflake"]
+my_cnx = snowflake.connector.connect(**snowflake_credentials)
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
