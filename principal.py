4d1c7d844376f6085c8b8127dab7a81ae1494a28
Marta N.- Data Analyst <116729581+Marta-BDA@users.noreply.github.com>
2023-08-23 11:28:05 +0200

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,4 +1,4 @@
-"""
+
 import streamlit
 import snowflake.connector
   
@@ -45,3 +45,4 @@ caption= product_caption
 streamlit.write('Price: ', df2[1])
 streamlit.write('Sizes Available: ',df2[2])
 streamlit.write(df2[3])
+"""
