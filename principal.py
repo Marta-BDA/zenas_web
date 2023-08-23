5289f5eeee35fd957c3e22d4b42cc356b13a8b7c
Marta N.- Data Analyst <116729581+Marta-BDA@users.noreply.github.com>
2023-08-23 11:04:25 +0200

    Update principal.py

--- principal.py
+++ principal.py
@@ -2,7 +2,7 @@
 import streamlit
 import snowflake.connector
 
-
+streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets[snowflake])
 my_cur = my_cnx.cursor()
